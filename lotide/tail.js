e18467f0309889c5129fdcaa2282b7e8e1360700
Zeenat <zeenatlawal82@gmail.com>
2022-04-17 19:20:09 +0100

    Assert tail works

--- lotide/tail.js
+++ lotide/tail.js
@@ -1,3 +1,4 @@
+const assertEqual = require("./assertEqual");
 let newArr = [];
 
 const tail = (arr) => {
@@ -7,3 +8,4 @@ const tail = (arr) => {
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3);
